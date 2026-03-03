7babc820919f3b2672c8dd2f54a025340f526e98
João Lima <jav.lima@campus.fct.unl.pt>
2026-03-03 17:42:54 +0000

    first commit

---
+++ src/main/kotlin/pt/unl/fct/iadi/orderprocessingplatform/IadiAssignment172943Application.kt
@@ -0,0 +1,11 @@
+package pt.unl.fct.iadi.orderprocessingplatform
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IadiAssignment172943Application
+
+fun main(args: Array<String>) {
+    runApplication<IadiAssignment172943Application>(*args)
+}
